79fd5116a95b1dd9bfd0732afd8d03970006091d
Kingsley Ihemelandu <kijuchihe@gmail.com>
2024-12-09 10:05:51 +0100

    Add client code to pms

--- client/src/app/(authenticated)/dashboard/page.tsx
+++ client/src/app/(authenticated)/dashboard/page.tsx
@@ -1,10 +1,9 @@
+'use client'
+
 import { useStore } from '@/shared/store/useStore'
 import Link from 'next/link'
 
-
-
 export default function Home() {
-
   const projects = useStore(state => state.projects)
   const threeProjects = projects?.slice(0, 3);
   const stats = [
@@ -41,7 +40,7 @@ export default function Home() {
           </Link>
         </div>
         <ul role="list" className="mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-          {threeProjects.map((project) => (
+          {threeProjects?.map((project) => (
             <li key={project.id} className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white dark:bg-dark-100 shadow">
               <div className="flex w-full items-center justify-between space-x-6 p-6">
                 <div className="flex-1 truncate">
@@ -58,7 +57,7 @@ export default function Home() {
                 <div className="-mt-px flex divide-x divide-gray-200">
                   <div className="flex w-0 flex-1">
                     <Link
-                      href={`/projects/${project}`}
+                      href={`/projects/${project.id}`}
                       className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-light"
                     >
                       View Details
